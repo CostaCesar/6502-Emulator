53ce6aee19ad0e4ec62e2941d0e9dd652fad8279
CostaCesar <caioplays@outlook.com>
2023-08-13 17:55:19 -0300

    ADD: Basic Reset

--- main.cpp
+++ main.cpp
@@ -18,10 +18,19 @@ struct CPU
     Byte F_Break : 1; 
     Byte F_OverFlow : 1; 
     Byte F_Negative : 1; 
+
+    void Reset()
+    {
+        ProgramCounter = 0xFFFC;
+        StackPointer = 0x0100;
+        F_Carry = F_Decimal = F_Zero = F_Interupt = F_Break = F_OverFlow = F_Negative = 0;
+        RegA = RegY = RegX = 0;
+    }
 };
 
 int main(int argc, char ** argv)
 {
     CPU processor;
+    processor.Reset();
     return 0;
 }
