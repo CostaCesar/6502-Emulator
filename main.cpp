27efca77012a2d848a5b54f8b9c1c0ef5f74831e
CostaCesar <caioplays@outlook.com>
2023-08-13 18:17:53 -0300

    ADD: Execute function

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 using Byte = unsigned char;
 using Word = unsigned short;
+
 struct Memory
 {
     static constexpr uint32_t MAX_SIZE = 1024 * 64;
@@ -14,7 +15,14 @@ struct Memory
         for(uint32_t i = 0; i < MAX_SIZE; i++)
             Data[i] = 0;
     }
+
+    /* Read 1 byte from memory */ 
+    Byte operator[] (uint32_t address) const
+    {
+        return Data[address];
+    }
 };
+
 struct CPU
 {
 
@@ -39,6 +47,21 @@ struct CPU
         RegA = RegY = RegX = 0;
         memory.Initialise();
     }
+
+    Byte FetchByte(uint32_t& cycles, Memory& memory)
+    {
+        Byte data = memory[ProgramCounter];
+        ProgramCounter++, cycles--;
+        return data;
+    }
+
+    void Execute(uint32_t cycles, Memory& memory)
+    {
+        for(cycles; cycles > 0; --cycles)
+        {
+            Byte instruction = FetchByte(cycles, memory);
+        }
+    }
 };
 
 int main(int argc, char ** argv)
@@ -46,5 +69,6 @@ int main(int argc, char ** argv)
     Memory memory;
     CPU processor;
     processor.Reset(memory);
+    processor.Execute( 2,memory);
     return 0;
 }
