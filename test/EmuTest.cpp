03637c965e290c9e087ea4075dc32ba34d8538a2
CostaCesar <caioplays@outlook.com>
2023-08-26 16:25:49 -0300

    ADD: LDA_ZP & LDA_ZPX testing

--- test/EmuTest.cpp
+++ test/EmuTest.cpp
@@ -19,36 +19,73 @@ public:
     }
 };
 
-TEST_F(M6502_Test, BasicProcessorTest)
+TEST_F(M6502_Test, LDA_Imediatate)
 {
-    // start - Hacked code
-    memory[0xFFFC] = CPU::INS_JSR;
-    memory[0xFFFD] = 0xAA;
-    memory[0xFFFE] = 0xAA;
-    memory[0xAAAA] = CPU::INS_LDA_IM;
-    memory[0xAAAB] = 0xA;
-    // end - Hacked code
+    // Given
+    memory[0xFFFC] = CPU::INS_LDA_IM;
+    memory[0xFFFD] = 0xA;
 
-    processor.Execute(8, memory);
-    EXPECT_EQ(processor.RegA, 0xA1);
+    // When
+    processor.Execute(2, memory);
+
+    // Execute
+    EXPECT_EQ(processor.RegA, 0xA);
+    EXPECT_FALSE(processor.F_Zero);
+    EXPECT_FALSE(processor.F_Negative);
 }
 
-#if 0
-int main(int argc, char ** argv)
+TEST_F(M6502_Test, LDA_ZeroPage)
 {
-    Memory memory;
-    CPU processor;
-    processor.Reset(memory);
-    
-    // start - Hacked code
-    memory[0xFFFC] = CPU::INS_JSR;
-    memory[0xFFFD] = 0xAA;
-    memory[0xFFFE] = 0xAA;
-    memory[0xAAAA] = CPU::INS_LDA_IM;
-    memory[0xAAAB] = 0xA;
-    // end - Hacked code
+    // Given
+    memory[0xFFFC] = CPU::INS_LDA_ZP;
+    memory[0xFFFD] = 0x0010;
+    memory[0x0010] = 0xA;
 
-    processor.Execute(8, memory);
-    return 0;
+    // When
+    processor.Execute(3, memory);
+
+    // Execute
+    EXPECT_EQ(processor.RegA, 0xA);
+    EXPECT_FALSE(processor.F_Zero);
+    EXPECT_FALSE(processor.F_Negative);
 }
-#endif
+
+TEST_F(M6502_Test, LDA_ZeroPage_OffsetX)
+{
+    // Given
+    processor.RegX = 0x5;
+    memory[0xFFFC] = CPU::INS_LDA_ZPX;
+    memory[0xFFFD] = 0x0010;
+    memory[0x0015] = 0xA;
+
+    // When
+    processor.Execute(4, memory);
+
+    // Execute
+    EXPECT_EQ(processor.RegA, 0xA);
+    EXPECT_FALSE(processor.F_Zero);
+    EXPECT_FALSE(processor.F_Negative);
+}
+
+TEST_F(M6502_Test, LDA_ZeroPage_OffsetX_Wrapping)
+{
+    // Given
+    processor.RegX = 0xFF;
+    memory[0xFFFC] = CPU::INS_LDA_ZPX;
+    memory[0xFFFD] = 0x0080;
+    memory[0x007F] = 0xA;
+
+    // When
+    processor.Execute(4, memory);
+
+    // Execute
+    EXPECT_EQ(processor.RegA, 0xA);
+    EXPECT_FALSE(processor.F_Zero);
+    EXPECT_FALSE(processor.F_Negative);
+}
+
+
+// INS_LDA_IM = 0xA9,  // 2 cycles: Load To RegA Imediate value
+// INS_LDA_ZP = 0xA5,  // 3 cyles: Load to RegA value from memory
+// INS_LDA_ZPX = 0xB5, // 4 cyles: Load to RegA value from memory + offset from RegX
+// INS_JSR = 0x20;     // 6 cyles: Branch to subroutine
