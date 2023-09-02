d22d084fe6ac4483b988c1bbb17637cf66fd2167
CostaCesar <caioplays@outlook.com>
2023-09-02 19:13:59 -0300

    ADD: STA tests

--- src/Base.h
+++ src/Base.h
@@ -9,29 +9,39 @@ enum Instruction: Byte
 {
     // >>>>> Memory <<<<<
     // > LDA
-    INS_LDA_IM = 0xA9,  // 2 cycles: Load to RegA imediate value
-    INS_LDA_ZP = 0xA5,  // 3 cycles: Load to RegA value from ZP-memory (0x0000 -> 0x00FF)
+    INS_LDA_IM  = 0xA9, // 2 cycles: Load to RegA imediate value
+    INS_LDA_ZP  = 0xA5, // 3 cycles: Load to RegA value from ZP-memory (0x0000 -> 0x00FF)
     INS_LDA_ZPX = 0xB5, // 4 cycles: Load to RegA value from ZP-memory + offset from RegX
-    INS_LDA_AB = 0xAD,  // 4 cycles: Load to RegA value from memory
+    INS_LDA_AB  = 0xAD, // 4 cycles: Load to RegA value from memory
     INS_LDA_ABX = 0xBD, // 4 cycles (+1 if cross boundary): Load to RegA value from memory + offset from RegX
     INS_LDA_ABY = 0xB9, // 4 cycles (+1 if cross boundary): Load to RegA value from memory + offset from RegY
     INS_LDA_IDX = 0xA1, // 6 cycles : Load to RegA value from position pointed by [ZP-memory + offset from RegX]
     INS_LDA_IDY = 0xB1, // 5 cycles (+1 if cross boundary): Load to RegA value from position ZP-memory + offset from RegY
     
     // > LDX
-    INS_LDX_IM = 0xA0,  // 2 cycles: Load to RegX imediate value
-    INS_LDX_ZP = 0xA4,  // 3 cycles: Load to RegX value from ZP-memory (0x0000 -> 0x00FF)
+    INS_LDX_IM  = 0xA0, // 2 cycles: Load to RegX imediate value
+    INS_LDX_ZP  = 0xA4, // 3 cycles: Load to RegX value from ZP-memory (0x0000 -> 0x00FF)
     INS_LDX_ZPY = 0xB4, // 4 cycles: Load to RegX value from ZP-memory + offset from RegY
-    INS_LDX_AB = 0xAC,  // 4 cycles: Load to RegX value from memory
+    INS_LDX_AB  = 0xAC, // 4 cycles: Load to RegX value from memory
     INS_LDX_ABY = 0xBC, // 4 cycles (+1 if cross boundary): Load to RegX value from memory + offset from RegY
 
     // > LDY
-    INS_LDY_IM = 0xA2,  // 2 cycles: Load to RegY imediate value
-    INS_LDY_ZP = 0xA6,  // 3 cycles: Load to RegY value from ZP-memory (0x0000 -> 0x00FF)
+    INS_LDY_IM  = 0xA2, // 2 cycles: Load to RegY imediate value
+    INS_LDY_ZP  = 0xA6, // 3 cycles: Load to RegY value from ZP-memory (0x0000 -> 0x00FF)
     INS_LDY_ZPX = 0xB6, // 4 cycles: Load to RegY value from ZP-memory + offset from RegX
-    INS_LDY_AB = 0xAE,  // 4 cycles: Load to RegY value from memory
+    INS_LDY_AB  = 0xAE, // 4 cycles: Load to RegY value from memory
     INS_LDY_ABX = 0xBE, // 4 cycles (+1 if cross boundary): Load to RegY value from memory + offset from RegX
 
+    // > STA
+    INS_STA_ZP  = 0x85, // 3 cycles: Store RegA value to ZP-memory (0x0000 -> 0x00FF)
+    INS_STA_ZPX = 0x95, // 4 cycles: Store RegA value to ZP-memory + offset from RegX
+    INS_STA_AB  = 0x8D, // 4 cycles: Store RegA value to memory
+    INS_STA_ABX = 0x9D, // 5 cycles: Store RegA value to memory + offset from RegX
+    INS_STA_ABY = 0x99, // 5 cycles: Store RegA value tomemory + offset from RegY
+    INS_STA_IDX = 0x81, // 6 cycles: Store RegA value to position pointed by [ZP-memory + offset from RegX]
+    INS_STA_IDY = 0x91, // 6 cycles: Store RegA value to position ZP-memory + offset from RegY
+    
+    
     // >>>>> Jump <<<<<
     INS_JSR = 0x20      // 6 cycles: Branch to subroutine
 };
---
+++ test/STA_Test.cpp
@@ -0,0 +1,163 @@
+#include "CPU_Model.h"
+
+class STA_Test : public M6502 {};
+
+TEST_F(STA_Test, STA_ZeroPage)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint32_t POSIT = 0x10;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_ZP;
+    memory[0xFFFD] = POSIT;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(3, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 3);
+    EXPECT_EQ(memory[POSIT], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_ZeroPage_OffsetX)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    const uint32_t POSIT = 0x10;
+    processor.RegA = VALUE;
+    processor.RegX = OFSET;
+    memory[0xFFFC] = INS_STA_ZPX;
+    memory[0xFFFD] = POSIT;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(4, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 4);
+    EXPECT_EQ(memory[POSIT + OFSET], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_ZeroPage_OffsetX_Wrapping)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    const uint16_t POSIT = 0xFA;
+    processor.RegA = VALUE;
+    processor.RegX = OFSET;
+    memory[0xFFFC] = INS_STA_ZPX;
+    memory[0xFFFD] = POSIT;
+    // When
+    uint32_t cycles_executed = processor.Execute(4, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 4);
+    EXPECT_EQ(memory[POSIT + OFSET], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_Absolute)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_AB;
+    memory[0xFFFD] = 0x80;
+    memory[0xFFFE] = 0xA1;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(4, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 4);
+    EXPECT_EQ(memory[0xA180], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_Absolute_OffsetX)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    processor.RegX = OFSET;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_ABX;
+    memory[0xFFFD] = 0xAA;
+    memory[0xFFFE] = 0xBB;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(5, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 5);
+    EXPECT_EQ(memory[0xBBAA + OFSET], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_Absolute_OffsetY)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    processor.RegY = OFSET;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_ABY;
+    memory[0xFFFD] = 0xAA;
+    memory[0xFFFE] = 0xBB;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(5, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 5);
+    EXPECT_EQ(memory[0xBBAA + OFSET], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_Indirect_OffsetX)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    const uint16_t ZPPOS = 0x20;
+    processor.RegX = OFSET;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_IDX;
+    memory[0xFFFD] = ZPPOS - OFSET;
+    memory[ZPPOS]  = 0xBA;
+    memory[ZPPOS+1]= 0xDC;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(6, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 6);
+    EXPECT_EQ(memory[0xDCBA], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
+TEST_F(STA_Test, STA_Indirect_OffsetY)
+{
+    // Given
+    const uint16_t VALUE = 0x40;
+    const uint16_t OFSET = 0xA;
+    const uint16_t ZPPOS = 0x20;
+    processor.RegX = OFSET;
+    processor.RegA = VALUE;
+    memory[0xFFFC] = INS_STA_IDX;
+    memory[0xFFFD] = ZPPOS;
+    memory[ZPPOS]  = 0xBA;
+    memory[ZPPOS+1]= 0xDC;
+
+    // When
+    uint32_t cycles_executed = processor.Execute(6, memory);
+
+    // Execute
+    EXPECT_EQ(cycles_executed, 6);
+    EXPECT_EQ(memory[0xDCBA+OFSET], VALUE);
+    VerifyUnusedFlags_ST(processor);
+}
+
