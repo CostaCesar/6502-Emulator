0f59b47e926f1e4044ed2a0135181abb9a05f8fb
CostaCesar <caioplays@outlook.com>
2024-06-07 13:37:43 -0300

    ADD: Emulator Interface

--- src/main_Emulator.cpp
+++ src/main_Emulator.cpp
@@ -1,8 +1,53 @@
 #include <fstream>
 #include <iostream>
+#include <chrono>
+#include <thread>
 #include "Base.h"
 #include "CPU.h"
 #include "Memory.h"
+
+#ifdef _WIN32
+#include <Windows.h>
+#define RESET_CURSOR() SetConsoleCursorPosition( GetStdHandle(STD_OUTPUT_HANDLE), (COORD ){0, 1})
+#elif __unix
+#include <unistd.h>
+#define RESET_CURSOR() printf("\033[0;1H")
+#endif
+
+struct EmulatorStatus
+{
+    bool autoRun = false; 
+    bool quit = false; 
+    bool stoped = true; 
+    uint32_t clock = 1000;
+};
+
+void Thread_CPU(CPU &Processor, Memory &Ram, EmulatorStatus& state)
+{
+    while (!state.quit)
+    {
+        RESET_CURSOR();
+            
+        printf("%c Stack Pointer: %#02x \n", (state.autoRun ? '*' : ' '), Processor.StackPointer);
+        printf("Address Pointer: %#04x \n", Processor.ProgramCounter);
+        printf("Flags:    (Negativ=%d) (Overflow=%d) (Break=%d) \n",
+                Processor.Flags.Negative, Processor.Flags.OverFlow, Processor.Flags.Break);
+        printf("(Zero=%d) (Decimal=%d) (Interupt=%d) (Carry=%d) \n",
+                Processor.Flags.Zero, Processor.Flags.Decimal, Processor.Flags.Interupt, Processor.Flags.Carry);
+        printf("Next Instruction: %s", Processor.Instructions[Ram[Processor.ProgramCounter]]);
+        printf("==================================================\n");
+        printf("[V] View Memory [S] View Stack [Z] View Zero Page \n");
+        printf("[ENTER] Execute [Q] Exit       [X] Toggle Autorun \n");
+           
+        if(!state.autoRun) // Wait for I/O
+            while (state.stoped);
+        else std::this_thread::sleep_for(std::chrono::milliseconds(state.clock));
+        state.stoped = true;
+
+        Processor.Execute(1, Ram);
+    }
+    return;
+}
 
 int main(int argc, char **argv)
 {
@@ -28,6 +73,7 @@ int main(int argc, char **argv)
 
     Ram.Initialise();
     CPU Processor(Chip, Address);
+    EmulatorStatus state;
 
     for(; inFile.peek() != EOF; Address++)
     {
@@ -35,7 +81,33 @@ int main(int argc, char **argv)
     }
     inFile.close();
 
-    Processor.Execute(100, Ram);
+    std::thread emulation(Thread_CPU, std::ref(Processor), std::ref(Ram), std::ref(state));
+    
+    uint8_t userInput = 0;
+    while (true)
+    {
+        userInput = std::getchar();
+        switch (userInput)
+        {
+        case 13: // ENTER
+        case 10: // New-line
+            if(state.autoRun) continue;
+            break;
+        case 'x':
+        case 'X':
+            state.autoRun = !state.autoRun;
+            break;
+        case 'q':
+        case 'Q':
+            state.quit = true;
+            emulation.join();
+            return 0;
+        default:
+            break;
+        }
+        state.stoped = false;
+    }
 
-    return 0;
+    emulation.join(); 
+    return 1;
 }
