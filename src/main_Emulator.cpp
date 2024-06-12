da72a3d0d5352cd3b57e3935fa112b75a7573d2f
CostaCesar <caioplays@outlook.com>
2024-06-11 22:06:47 -0300

    ADD: Complete Memory View

--- src/main_Emulator.cpp
+++ src/main_Emulator.cpp
@@ -20,6 +20,8 @@
 #define RESET_CURSOR(x, y) printf("\033[%d;%dH", y, x)
 #endif
 
+#define HIDE_INPUT std::cout << "                              " << std::endl
+#define CONFIRM_ACTION std::cout << "Press ENTER to continue" << std::endl; std::getchar();
 struct EmulatorStatus
 {
     bool autoRun = false; 
@@ -33,13 +35,13 @@ struct EmulatorStatus
     std::condition_variable signal;
 } G_state;
 
-void printCPU(CPU &Processor, const Memory &Ram)
+void Screen_CPU(CPU &Processor, const Memory &Ram)
 {
     RESET_CURSOR(0, 1);
 
     printf("%c Stack Pointer: %#02x \n", (G_state.autoRun ? '*' : ' '), Processor.StackPointer);
     printf("Address Pointer: %#04x \n", Processor.ProgramCounter);
-    printf("Flags:    (Negativ=%d) (Overflow=%d) (Break=%d) \n",
+    printf("Flags:   (Negativ=%d) (Overflow=%d) (Break=%d) \n",
            Processor.Flags.Negative, Processor.Flags.OverFlow, Processor.Flags.Break);
     printf("(Zero=%d) (Decimal=%d) (Interupt=%d) (Carry=%d) \n",
            Processor.Flags.Zero, Processor.Flags.Decimal, Processor.Flags.Interupt, Processor.Flags.Carry);
@@ -48,32 +50,94 @@ void printCPU(CPU &Processor, const Memory &Ram)
     printf("[V] View Memory [S] View Stack [Z] View Zero Page \n");
     printf("[ENTER] Execute [Q] Exit       [X] Toggle Autorun \n");
     printf("==================================================\n");
-    printf("[User@Emulator]$                                    ");
+    printf("[User@Emulator]$ ");
 
-    RESET_CURSOR(17, 10);
     std::flush(std::cout);
     return;
 }
-void printMemory(const Memory& ram, Word startPos = 0, Word endPos = 0xFFFF)
+void Screen_Memory(const Memory& Ram, Word StartPos = 0, Word EndPos = 0xFFFF)
 {
     CLEAR_SCREEN;
     RESET_CURSOR(0, 0);
+    
+    if(EndPos < StartPos)
+    {
+        std::cout << "END OF MEMORY" << std::endl;
+        return;
+    }
 
-    for(Word i = startPos; i <= endPos; i+=0x10)
+    for(Word i = StartPos; i < EndPos; i+=0x10)
     {
         printf("0x%04x", i);
         for(Byte j = 0; j < 0x10; j++)
         {
-            printf(" %02x", ram[i+j]);
+            printf(" %02x", Ram[i+j]);
         }
         printf("\n");
     }
     std::flush(std::cout);
 
-    std::cout << "Press ENTER to continue" << std::endl;
-    std::getchar();
+    return;
+}
+void Paged_Screen_Memory(const Memory& Ram)
+{
+    static Word page = 0;
+    bool validInput = true;
 
-    CLEAR_SCREEN;
+    while(true)
+    {
+        if(validInput)
+            Screen_Memory(Ram, page*0x100, (page + 1)*0x100);
+        else RESET_CURSOR(0, 16);
+        validInput = true;
+        
+        printf("===================================\n");
+        printf("[A] Previous           [D] Next    \n");
+        printf("[S <address>] Go to    [Q] Exit    \n");
+        printf("===================================\n");
+        printf("[User@Emulator]$ ");
+        std::flush(std::cout);
+
+        uint8_t userInput = std::getchar();
+        RESET_CURSOR(17, 20);
+        HIDE_INPUT;
+        
+        // Remove leftorver '\n'
+        if(userInput != 13 && userInput != 10 && userInput != 's' && userInput != 'S')
+            std::getchar();
+
+        switch (userInput)
+        {
+        case 'a':
+        case 'A':
+            page--;
+            break;
+        case 'd':
+        case 'D':
+            page++;
+            break;
+        case 's':
+        case 'S':
+            // Redo S (in case of early ENTER)
+            RESET_CURSOR(17, 20);
+            std::cout << (char)userInput << ' ';
+            std::cout.flush();
+
+            std::cin >> std::hex >> page >> std::dec;
+            // Wrap as if it was Byte
+            page = (page / 0xFF) % 0x100;
+            // Consume leftover '\n'
+            std::getchar();
+            break;
+        case 'q':
+        case 'Q':
+            CLEAR_SCREEN;
+            return;
+        default:
+            validInput = false;
+            continue;
+        }
+    }
     return;
 }
 
@@ -81,14 +145,17 @@ void Thread_CPU(CPU &Processor, Memory &Ram)
 {
     while (!G_state.quit)
     {
-        printCPU(Processor, Ram);
+        Screen_CPU(Processor, Ram);
         
         std::unique_lock<std::mutex> lock(G_state.mutex_emulator);
-        if(!G_state.autoRun) // Wait for I/O Mode 
-            { G_state.signal.wait(lock, []{ return G_state.running; }); }
-        else // Autorun Mode
-        { 
+        if(!G_state.autoRun) 
+        {   // Step Mode 
+            G_state.signal.wait(lock, []{ return G_state.running; });
+        }
+        else 
+        {   // Autorun Mode
             std::this_thread::sleep_for(std::chrono::milliseconds(G_state.clock));
+            // If viewing others screens (memory, etc), wait for it to close
             G_state.signal.wait(lock, []{ return !G_state.viewing; }); 
         }
         G_state.running = false;
@@ -126,7 +193,7 @@ int main(int argc, char **argv)
     Ram.Initialise();
     CPU Processor(Chip, Address);
 
-    for(; inFile.peek() != EOF; Address++)
+    for(Address; inFile.peek() != EOF; Address++)
     {
         inFile.read((char *) &Ram[Address], sizeof(Byte));
     }
@@ -135,6 +202,18 @@ int main(int argc, char **argv)
     std::thread emulation(Thread_CPU, std::ref(Processor), std::ref(Ram));
     
     uint8_t userInput = 0;
+    auto OnScreenExit = [&Processor, &Ram]
+    {
+        CLEAR_SCREEN;
+        Screen_CPU(Processor, Ram);
+        // Pause for autorun Mode
+        if(G_state.autoRun)
+            std::this_thread::sleep_for(std::chrono::milliseconds(G_state.clock));
+        // Resume CPU thread
+        G_state.viewing = false;
+        G_state.signal.notify_all();
+    };
+
     while (true)
     {
         if(G_state.quit)
@@ -145,10 +224,13 @@ int main(int argc, char **argv)
         }
 
         userInput = std::getchar();
+        RESET_CURSOR(17, 10);
+        HIDE_INPUT;
+
         // Remove leftorver '\n'
         if(userInput != 13 && userInput != 10)
             std::getchar();
-
+        
         switch (userInput)
         {
         case 13: // ENTER
@@ -162,22 +244,32 @@ int main(int argc, char **argv)
         case 'z':
         case 'Z':
             G_state.viewing = true;
-            printMemory(Ram, 0, 0xFF);
-            printCPU(Processor, Ram);
-            std::this_thread::sleep_for(std::chrono::milliseconds(G_state.clock));
-            G_state.viewing = false;
-            G_state.signal.notify_all();
+            Screen_Memory(Ram, 0, 0xFF);
+            CONFIRM_ACTION;
+            OnScreenExit();
+            continue;
+        case 's':
+        case 'S':
+            G_state.viewing = true;
+            Screen_Memory(Ram, 0x100, 0x1FF);
+            CONFIRM_ACTION;
+            OnScreenExit();
+            continue;
+        case 'v':
+        case 'V':
+            G_state.viewing = true;
+            Paged_Screen_Memory(Ram);
+            OnScreenExit();
             continue;
         case 'q':
         case 'Q':
             G_state.quit = true;
             break;
         default:
-            RESET_CURSOR(17, 10);
             continue;
         }
         
-        // Singal emulator to execute command
+        // Singal emulator to execute command (Step Mode)
         std::lock_guard<std::mutex> lock(G_state.mutex_emulator);
         G_state.running = true;
         G_state.signal.notify_all();
